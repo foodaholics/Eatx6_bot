43c8776eb1acc585dea17f5d3eb899679859b86b
Pin Lin <moneycat711@gmail.com>
2018-08-22 09:59:21 +0800

    使用者想要查看折價券有什麼

--- tasks/viewcoupon_task.py
+++ tasks/viewcoupon_task.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 from tasks.base_task import BaseTask
 
@@ -17,7 +18,7 @@ class ViewCouponTask(BaseTask):
 
 
         # TODO: 判斷是否觸發
-        result = 'text' in msg and msg['text'] in ['/coupon']
+        result = ('text' in msg and msg['text'] in ['/coupon']) or users[chat_id]['status'] == '/coupon'
 
 
         # 回傳是否觸發的結果
@@ -27,6 +28,8 @@ class ViewCouponTask(BaseTask):
         # 把 self.bot 簡寫成 bot
         bot = self.bot
 
+        print("[ViewCouponTask] main")
+
         # 取得使用者的資訊
         try:
             content_type, chat_type, chat_id = telepot.glance(msg)
@@ -34,5 +37,18 @@ class ViewCouponTask(BaseTask):
             query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
 
         # TODO: 實作實際的功能
-        bot.sendMessage(chat_id, "查看折價券的功能建置中！")
-        print("[ViewCouponTask] main")
+        if users[chat_id]['status'] != '/coupon':
+            # 設定狀態
+            users[chat_id]['status'] = '/coupon'
+
+            # 顯示目前有哪些商店支援優惠券
+            keyboard = [[]]
+            for key in self.data['stores'].keys():
+                if len(keyboard[-1]) < 3:
+                    keyboard[-1].append(InlineKeyboardButton(text=key, callback_data=key))
+                else:
+                    keyboard.append([InlineKeyboardButton(text=key, callback_data=key)])
+
+            bot.sendMessage(chat_id, "我目前有這些餐廳的優惠券～你想看看哪一家的？", reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard))
+        else:
+            bot.sendMessage(chat_id, "想看折價券齁？")
